544c796d12764c3bf82f615d25fc304f84e47098
mudoker <huuquoc7603@gmail.com>
2026-06-16 07:02:53 +0700

    feat(jazzlounge): add duration metadata to jazz chord progressions

--- internal/viewmodel/jazzlounge/chord.go
+++ internal/viewmodel/jazzlounge/chord.go
@@ -8,6 +8,7 @@ type JazzChord struct {
 	RootOffset int
 	Intervals  []int // offsets from root
 	Name       string
+	Duration   int // duration in ticks
 }
 
 func (c JazzChord) GenerateVoicing(size int) []int {
@@ -45,71 +46,67 @@ func GenerateDynamicProgression() ([]JazzChord, bool) {
 	case 0: // Major ii-V-I standard (8 chords)
 		isMinorProg = false
 		progression = []JazzChord{
-			{RootOffset: 2, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},     // ii (Dm9)
-			{RootOffset: 7, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13"}, // V (G13)
-			{RootOffset: 0, Intervals: []int{0, 4, 7, 11, 14}, Name: "maj9"},   // I (Cmaj9)
-			{RootOffset: 9, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt"},   // VI (A7alt)
-			{RootOffset: 2, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},     // ii (Dm9)
-			{RootOffset: 7, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13"}, // V (G13)
-			{RootOffset: 4, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},     // iii (Em9)
-			{RootOffset: 9, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt"},   // VI (A7alt)
+			{RootOffset: 2, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},     // ii (Dm9)
+			{RootOffset: 7, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13", Duration: 16}, // V (G13)
+			{RootOffset: 0, Intervals: []int{0, 4, 7, 11, 14}, Name: "maj9", Duration: 32},   // I (Cmaj9)
+			{RootOffset: 9, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt", Duration: 16},   // VI (A7alt)
+			{RootOffset: 2, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},     // ii (Dm9)
+			{RootOffset: 7, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13", Duration: 16}, // V (G13)
+			{RootOffset: 4, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},     // iii (Em9)
+			{RootOffset: 9, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt", Duration: 16},   // VI (A7alt)
 		}
 	case 1: // Minor Autumn/Noir standard (8 chords)
 		isMinorProg = true
 		progression = []JazzChord{
-			{RootOffset: 5, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},      // iv (Fm9)
-			{RootOffset: 10, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13"}, // bVII (Bb13)
-			{RootOffset: 3, Intervals: []int{0, 4, 7, 11, 14}, Name: "maj9"},    // bIII (Ebmaj9)
-			{RootOffset: 8, Intervals: []int{0, 4, 7, 11, 14}, Name: "maj9"},    // bVI (Abmaj9)
-			{RootOffset: 2, Intervals: []int{0, 3, 6, 10, 13}, Name: "m7b5"},    // iiø (Dø7)
-			{RootOffset: 7, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt"},    // V7alt (G7alt)
-			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},      // i (Cm9)
-			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},      // i (Cm9)
+			{RootOffset: 5, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},      // iv (Fm9)
+			{RootOffset: 10, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13", Duration: 16}, // bVII (Bb13)
+			{RootOffset: 3, Intervals: []int{0, 4, 7, 11, 14}, Name: "maj9", Duration: 16},    // bIII (Ebmaj9)
+			{RootOffset: 8, Intervals: []int{0, 4, 7, 11, 14}, Name: "maj9", Duration: 16},    // bVI (Abmaj9)
+			{RootOffset: 2, Intervals: []int{0, 3, 6, 10, 13}, Name: "m7b5", Duration: 16},    // iiø (Dø7)
+			{RootOffset: 7, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt", Duration: 16},    // V7alt (G7alt)
+			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32},      // i (Cm9)
+			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},      // i (Cm9)
 		}
 	case 2: // 12-Bar Jazz Blues (12 chords)
 		isMinorProg = rand.Float64() < 0.5
 		if isMinorProg {
 			progression = []JazzChord{
-				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},    // i (Cm9)
-				{RootOffset: 5, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},    // iv (Fm9)
-				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},    // i (Cm9)
-				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},    // i (Cm9)
-				{RootOffset: 5, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},    // iv (Fm9)
-				{RootOffset: 5, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},    // iv (Fm9)
-				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},    // i (Cm9)
-				{RootOffset: 8, Intervals: []int{0, 4, 7, 11, 14}, Name: "maj9"},  // bVI (Abmaj9)
-				{RootOffset: 2, Intervals: []int{0, 3, 6, 10, 13}, Name: "m7b5"},  // iiø (Dø7)
-				{RootOffset: 7, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt"},  // V7alt (G7alt)
-				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},    // i (Cm9)
-				{RootOffset: 7, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt"},  // V7alt (G7alt)
+				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},   // i (Cm9)
+				{RootOffset: 5, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},   // iv (Fm9)
+				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32},   // i (Cm9)
+				{RootOffset: 5, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32},   // iv (Fm9)
+				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32},   // i (Cm9)
+				{RootOffset: 8, Intervals: []int{0, 4, 7, 11, 14}, Name: "maj9", Duration: 16}, // bVI (Abmaj9)
+				{RootOffset: 2, Intervals: []int{0, 3, 6, 10, 13}, Name: "m7b5", Duration: 16}, // iiø (Dø7)
+				{RootOffset: 7, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt", Duration: 16}, // V7alt (G7alt)
+				{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},   // i (Cm9)
+				{RootOffset: 7, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt", Duration: 16}, // V7alt (G7alt)
 			}
 		} else {
 			progression = []JazzChord{
-				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9"},     // I9 (C9)
-				{RootOffset: 5, Intervals: []int{0, 4, 7, 10, 14}, Name: "9"},     // IV9 (F9)
-				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9"},     // I9 (C9)
-				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9"},     // I9 (C9)
-				{RootOffset: 5, Intervals: []int{0, 4, 7, 10, 14}, Name: "9"},     // IV9 (F9)
-				{RootOffset: 5, Intervals: []int{0, 4, 7, 10, 14}, Name: "9"},     // IV9 (F9)
-				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9"},     // I9 (C9)
-				{RootOffset: 9, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt"},  // VI7alt (A7alt)
-				{RootOffset: 2, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"},     // ii (Dm9)
-				{RootOffset: 7, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13"}, // V (G13)
-				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9"},     // I9 (C9)
-				{RootOffset: 7, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13"}, // V (G13)
+				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9", Duration: 16},     // I9 (C9)
+				{RootOffset: 5, Intervals: []int{0, 4, 7, 10, 14}, Name: "9", Duration: 16},     // IV9 (F9)
+				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9", Duration: 32},     // I9 (C9)
+				{RootOffset: 5, Intervals: []int{0, 4, 7, 10, 14}, Name: "9", Duration: 32},     // IV9 (F9)
+				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9", Duration: 32},     // I9 (C9)
+				{RootOffset: 9, Intervals: []int{0, 4, 8, 10, 13}, Name: "7alt", Duration: 16},  // VI7alt (A7alt)
+				{RootOffset: 2, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 16},     // ii (Dm9)
+				{RootOffset: 7, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13", Duration: 16}, // V (G13)
+				{RootOffset: 0, Intervals: []int{0, 4, 7, 10, 14}, Name: "9", Duration: 16},     // I9 (C9)
+				{RootOffset: 7, Intervals: []int{0, 4, 7, 10, 14, 21}, Name: "13", Duration: 16}, // V (G13)
 			}
 		}
 	case 3: // Modal So What / Impressions (8 chords)
 		isMinorProg = true
 		progression = []JazzChord{
-			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"}, // i (Cm9)
-			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"}, // i (Cm9)
-			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"}, // i (Cm9)
-			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"}, // i (Cm9)
-			{RootOffset: 1, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"}, // bii (Dbm9)
-			{RootOffset: 1, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"}, // bii (Dbm9)
-			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"}, // i (Cm9)
-			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9"}, // i (Cm9)
+			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32}, // i (Cm9)
+			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32}, // i (Cm9)
+			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32}, // i (Cm9)
+			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32}, // i (Cm9)
+			{RootOffset: 1, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32}, // bii (Dbm9)
+			{RootOffset: 1, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32}, // bii (Dbm9)
+			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32}, // i (Cm9)
+			{RootOffset: 0, Intervals: []int{0, 3, 7, 10, 14}, Name: "m9", Duration: 32}, // i (Cm9)
 		}
 	}
 	return progression, isMinorProg
